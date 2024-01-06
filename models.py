9a3a1eff6c3b6f36f871a487e005e3f7a728a330
marie <mariegacheri@gmail.com>
2024-01-06 16:55:38 +0300

    Updated models.py

--- models.py
+++ models.py
@@ -1,66 +1,54 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+# models.py
+from flask_sqlalchemy import SQLAlchemy
 
-Base = declarative_base()
+db = SQLAlchemy()
 
-class Restaurant(Base):
+class Review(db.Model):
+    __tablename__ = 'reviews'
+    id = db.Column(db.Integer, primary_key=True)
+    star_rating = db.Column(db.Integer)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    restaurant = db.relationship('Restaurant', back_populates='reviews')
+    customer = db.relationship('Customer', back_populates='reviews')
+
+class Restaurant(db.Model):
     __tablename__ = 'restaurants'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    price = Column(Integer)
-    reviews = relationship('Review', back_populates='restaurant')
-    customers = relationship('Customer', secondary='reviews', back_populates='restaurants')
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    price = db.Column(db.Integer)
+    reviews = db.relationship('Review', back_populates='restaurant')
 
     @classmethod
     def fanciest(cls):
-        return session.query(cls).order_by(cls.price.desc()).first()
+        return cls.query.order_by(cls.price.desc()).first()
 
     def all_reviews(self):
         return [review.full_review() for review in self.reviews]
 
-class Review(Base):
-    __tablename__ = 'reviews'
-    id = Column(Integer, primary_key=True)
-    star_rating = Column(Integer)
-    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
-    customer_id = Column(Integer, ForeignKey('customers.id'))
-    restaurant = relationship('Restaurant', back_populates='reviews')
-    customer = relationship('Customer', back_populates='reviews')
-
-    def full_review(self):
-        return f"Review for {self.restaurant.name} by {self.customer.full_name()}: {self.star_rating} stars."
-
-class Customer(Base):
+class Customer(db.Model):
     __tablename__ = 'customers'
-    id = Column(Integer, primary_key=True)
-    first_name = Column(String)
-    last_name = Column(String)
-    reviews = relationship('Review', back_populates='customer')
-    restaurants = relationship('Restaurant', secondary='reviews', back_populates='customers')
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    reviews = db.relationship('Review', back_populates='customer')
 
     def full_name(self):
-        return f"{self.first_name} {self.last_name}"
+        return f'{self.first_name} {self.last_name}'
 
     def favorite_restaurant(self):
-        return max(self.restaurants, key=lambda restaurant: restaurant.reviews)
+        return max(self.reviews, key=lambda r: r.star_rating).restaurant
 
     def add_review(self, restaurant, rating):
-        review = Review(restaurant=restaurant, customer=self, star_rating=rating)
-        session.add(review)
-        session.commit()
-        return review
+        new_review = Review(restaurant=restaurant, customer=self, star_rating=rating)
+        db.session.add(new_review)
+        db.session.commit()
 
     def delete_reviews(self, restaurant):
         reviews_to_delete = [review for review in self.reviews if review.restaurant == restaurant]
         for review in reviews_to_delete:
-            session.delete(review)
-        session.commit()
+            db.session.delete(review)
+        db.session.commit()
 
-    @classmethod
-    def find_by_name(cls, full_name):
-        first_name, last_name = full_name.split()
-        return session.query(cls).filter_by(first_name=first_name, last_name=last_name).first()
-
-    @classmethod
-    def find_all_by_given_name(cls, given_name):
-        return session.query(cls).filter(cls.first_name == given_name).all()
+# Add this to the end of the file
+db.create_all()
